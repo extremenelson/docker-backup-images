c577fff20c1af2e79b64965e35b058d01d84134b
Alex Nelson <anelson@ipponusa.com>
2018-03-17 08:51:06 -0400

    Updated code to have save as a separate method and save in current directory.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,43 @@
 use std::process::Command;
 
+#[derive(PartialEq, Default, Clone, Debug)]
+struct Image {
+    name: String,
+    tag: String,
+    hash: String,
+}
+
+fn save() {
+   let output = Command::new("docker")
+            .args(&["images"])
+            .output()
+            .expect("Failed to execute process");
+
+    let images = String::from_utf8_lossy(&output.stdout);
+    let to_process = images.lines().skip(1);
+
+    to_process
+        .map(|line| {
+            let columns: Vec<&str> = line.split_whitespace().collect();
+            let parsed_name: Vec<&str> = columns[0].split("/").collect();
+            Image {
+                name: parsed_name[parsed_name.len()-1].to_string(),
+                tag: columns[1].to_string(),
+                hash: columns[2].to_string(),
+            }
+        })
+        .for_each(|image| {
+            let save_name: String = image.name + "-" + &image.tag + ".tgz";
+            println!("Saving image with hash {} to {}", image.hash, save_name);
+            let save = Command::new("docker")
+                .args(&["save", "-o", &save_name, &image.hash])
+                .output()
+                .expect("Failed to save image.");
+
+            println!("{}", String::from_utf8_lossy(&save.stdout));
+        });
+}
+
 fn main() {
-    println!("Hello, world!");
-
-    let output = if cfg!(target_os = "windows") {
-        Command::new("cmd")
-            .args(&["/c", "echo helo"])
-            .output()
-            .expect("failed to execute process")
-    } else {
-        Command::new("sh")
-            .args(&["/C", "echo hello"])
-            .output()
-            .expect("Failed to execute process")
-    };
-
-    let hello = output.stdout;
+    save();
 }
